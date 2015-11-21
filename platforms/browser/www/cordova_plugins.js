93a8be36aa5280aae212a7f6d0e1c0124fb45cc2
jgluhov <jgluhov@iMac-jgluhov.local>
2015-11-21 13:40:48 +0300

    make up sign up and create sign in and home

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,5 +1,22 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-screen-orientation/www/screenorientation.js",
+        "id": "cordova-plugin-screen-orientation.screenorientation",
+        "pluginId": "cordova-plugin-screen-orientation",
+        "clobbers": [
+            "cordova.plugins.screenorientation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-keyboard/www/keyboard.js",
+        "id": "cordova-plugin-keyboard.keyboard",
+        "pluginId": "cordova-plugin-keyboard",
+        "clobbers": [
+            "window.Keyboard"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {}
